3c7b2edc5697fd2767b49dd1e55cdc0c177c01cd
Kryspin Musiol <kryspin.musiol@gmail.com>
2018-12-04 00:48:08 +0100

    init

---
+++ src/main/java/musiol/kryspin/springpetclinicbreakdown/SpringPetClinicBreakdownApplication.java
@@ -0,0 +1,12 @@
+package musiol.kryspin.springpetclinicbreakdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetClinicBreakdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetClinicBreakdownApplication.class, args);
+    }
+}
