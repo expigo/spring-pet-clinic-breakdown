bbe8ef73da7fbc507077cab20147681b63816be1
Kryspin Musiol <kryspin.musiol@gmail.com>
2018-12-08 19:50:37 +0100

    refactoring to common crud interface. closes #4

--- pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/services/OwnerService.java
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/services/OwnerService.java
@@ -2,15 +2,7 @@ package musiol.kryspin.springpetclinicbreakdown.services;
 
 import musiol.kryspin.springpetclinicbreakdown.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
