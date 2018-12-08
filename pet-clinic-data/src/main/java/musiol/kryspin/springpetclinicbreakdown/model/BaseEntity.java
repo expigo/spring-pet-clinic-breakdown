1f361a56365ea842aa6b9e619069d7d0712f2500
Kryspin Musiol <kryspin.musiol@gmail.com>
2018-12-08 01:52:41 +0100

    base entity added

---
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package musiol.kryspin.springpetclinicbreakdown.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/model/Person.java
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/model/Person.java
@@ -1,6 +1,6 @@
 package musiol.kryspin.springpetclinicbreakdown.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
