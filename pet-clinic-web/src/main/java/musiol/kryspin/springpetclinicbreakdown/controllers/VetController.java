72db9804ab0bb196b28ab8d8cbf77385418e436b
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-01-08 22:50:37 +0100

    Adding the feature of presenting the vets list.

--- pet-clinic-web/src/main/java/musiol/kryspin/springpetclinicbreakdown/controllers/VetController.java
+++ pet-clinic-web/src/main/java/musiol/kryspin/springpetclinicbreakdown/controllers/VetController.java
@@ -1,15 +1,24 @@
 package musiol.kryspin.springpetclinicbreakdown.controllers;
 
 
+import musiol.kryspin.springpetclinicbreakdown.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
