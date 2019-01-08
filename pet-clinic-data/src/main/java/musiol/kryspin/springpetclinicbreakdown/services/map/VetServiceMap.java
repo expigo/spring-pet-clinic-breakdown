66044c93d973d1ee401ef628cc17fd59c834b007
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-01-08 21:35:52 +0100

    Adding bootstrap for data.

--- pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package musiol.kryspin.springpetclinicbreakdown.services.map;
 
 import musiol.kryspin.springpetclinicbreakdown.model.Vet;
-import musiol.kryspin.springpetclinicbreakdown.services.CrudService;
+import musiol.kryspin.springpetclinicbreakdown.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
