32004c5af77645737ea4afcf44315d64d11cc147
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-02-28 23:31:36 +0100

    Add PetType to CommandLineRunner bootstrap class

--- pet-clinic-web/src/main/java/musiol/kryspin/springpetclinicbreakdown/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/musiol/kryspin/springpetclinicbreakdown/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package musiol.kryspin.springpetclinicbreakdown.bootstrap;
 
 import musiol.kryspin.springpetclinicbreakdown.model.Owner;
+import musiol.kryspin.springpetclinicbreakdown.model.PetType;
 import musiol.kryspin.springpetclinicbreakdown.model.Vet;
 import musiol.kryspin.springpetclinicbreakdown.services.OwnerService;
+import musiol.kryspin.springpetclinicbreakdown.services.PetTypeService;
 import musiol.kryspin.springpetclinicbreakdown.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {  // will get to run when the whole Spring app context is up and ready
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        final PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        final PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Kryspin");
