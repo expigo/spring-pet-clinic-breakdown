5d5bad1607d2aa7736b2eb31d6d90ba734931374
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-02-28 23:13:40 +0100

    Add PetTypeService

--- pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/model/PetType.java
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/model/PetType.java
@@ -1,6 +1,6 @@
 package musiol.kryspin.springpetclinicbreakdown.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
---
+++ pet-clinic-data/src/main/java/musiol/kryspin/springpetclinicbreakdown/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package musiol.kryspin.springpetclinicbreakdown.services;
+
+import musiol.kryspin.springpetclinicbreakdown.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
